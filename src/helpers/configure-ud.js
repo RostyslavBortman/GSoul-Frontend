f6513c36731ae53666c759c21f5facf545c9389d
RostyslavBortman <rostyslav@horizenlabs.io>
2022-09-09 01:29:54 +0100

    make it better

--- src/helpers/configure-ud.js
+++ src/helpers/configure-ud.js
@@ -2,6 +2,6 @@ import UAuth from '@uauth/js'
 
 export const uauth = new UAuth({
     clientID: "d412aa56-0f96-4012-9c9d-aea918fc888f",
-    redirectUri: "https://gsoul-frontend.herokuapp.com",
+    redirectUri: "https://gsoul-view.herokuapp.com/",
     scope: "openid wallet"
 }); 
